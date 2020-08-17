09a4fb7e99ba040df68a5c5e5c889f7e5cc33877
firmo holanda <firmo@viize.com>
2020-08-17 19:04:55 -0300

    edit book table colum name

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+
 import Book from '../components/Book';
 
 const BooksList = ({ books }) => (
   <table>
     <thead>
       <tr>
-        <th>Book ID</th>
-        <th>Title</th>
-        <th>Category</th>
+        <th>id</th>
+        <th>title</th>
+        <th>category</th>
       </tr>
     </thead>
     <tbody>
