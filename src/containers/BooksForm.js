4cd9644107cdeae8cdd501a879ce0031c08f4f5d
firmo holanda <firmo@viize.com>
2020-08-17 19:04:23 -0300

    edit categories names

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
 const BooksForm = () => {
-  const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+  const categories = ['action', 'biography', 'history', 'horror', 'kids', 'learning', 'sci-fi'];
   return (
     <form>
-      <input type="text" name="book" placeholder="New book" />
+      <input type="text" name="book" placeholder="new book" />
       <select>
         {categories.map((category) => (
           <option key={category} value={category}>{category}</option>
         ))}
       </select>
-      <button type="submit">Create new book</button>
+      <button type="submit">create new book</button>
     </form>
   );
 };
