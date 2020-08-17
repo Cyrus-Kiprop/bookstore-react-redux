d4fb046deccc0fa7cffa14ea140c10b1da8ec5a3
firmo holanda <firmo@viize.com>
2020-08-17 18:42:21 -0300

    edit app component with BooksList and BooksForm components

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import logo from '../assets/images/bookstore-logo.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a href="!#" className="App-link" target="_blank" rel="noopener noreferrer">
-          React Bookstore
-        </a>
-      </header>
-    </div>
-  );
-}
+import BooksList from '../containers/BooksList';
+import BooksForm from '../containers/BooksForm';
+
+const App = () => (
+  <div className="app">
+    <BooksList />
+    <BooksForm />
+  </div>
+);
 
 export default App;
