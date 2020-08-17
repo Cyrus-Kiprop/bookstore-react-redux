50b8bdc2ee6776aa57298399a228140faec776c6
firmo holanda <firmo@viize.com>
2020-08-17 18:52:57 -0300

    create fake books and initialize redux store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './assets/styles/index.css';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import App from './components/App';
+import rootReducer from './reducers';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/styles/index.css';
+
+var faker = require('faker');
+
+const booksArray = [
+  {
+    id: (Math.random() * (10 ** 9)),
+    title: faker.lorem.words(),
+    category: 'Action',
+  },
+  {
+    id: (Math.random() * (10 ** 9)),
+    title: faker.lorem.words(),
+    category: 'Learning',
+  },
+  {
+    id: (Math.random() * (10 ** 9)),
+    title: faker.lorem.words(),
+    category: 'Horror',
+  },
+];
+
+const initState = {
+  books: booksArray,
+};
+
+const store = createStore(rootReducer, initState);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root'),
 );
