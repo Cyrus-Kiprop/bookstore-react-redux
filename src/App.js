a639fcec48f9c8438fb879fceb0682edbe47a117
firmo holanda <firmo@viize.com>
2020-08-17 17:18:29 -0300

    add eslint airbnb

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from "react";
-import logo from "./bookstore-logo.png";
-import "./App.css";
+import React from 'react';
+import logo from './bookstore-logo.png';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <a className="App-link" target="_blank" rel="noopener noreferrer">
+        <a href="!#" className="App-link" target="_blank" rel="noopener noreferrer">
           React Bookstore
         </a>
       </header>
