20492633bbe5370d85c88af558ea10938afcf702
kip <cyruskiprop254@gmail.com>
2020-08-18 20:01:14 +0300

    refactor code

--- src/index.js
+++ src/index.js
@@ -1,40 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 
-import App from './components/App';
-import rootReducer from './reducers';
+import App from './containers/App';
+import configureStore from './configureStore';
+import initialState from './rawData';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles/index.css';
 
-var faker = require('faker');
-
-const booksArray = [
-  {
-    id: (Math.floor(Math.random() * 99) + 1),
-    title: faker.lorem.words(),
-    category: 'action',
-  },
-  {
-    id: (Math.floor(Math.random() * 99) + 1),
-    title: faker.lorem.words(),
-    category: 'biography',
-  },
-  {
-    id: (Math.floor(Math.random() * 99) + 1),
-    title: faker.lorem.words(),
-    category: 'history',
-  },
-];
-
-const initState = {
-  books: booksArray,
-};
-
-const store = createStore(rootReducer, initState);
+const store = configureStore(initialState);
 
 ReactDOM.render(
   <Provider store={store}>
