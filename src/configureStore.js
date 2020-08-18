763bc5593c42de58241965e865d201ad40c37f5a
kip <cyruskiprop254@gmail.com>
2020-08-18 19:59:18 +0300

    configure store with middleware and modularize database away from index.js file

---
+++ src/configureStore.js
@@ -0,0 +1,14 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
+import rootReducer from './reducers/index';
+
+function configureStore(initialState) {
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // add support for Redux dev tools
+  return createStore(
+    rootReducer,
+    initialState,
+    composeEnhancers(applyMiddleware(reduxImmutableStateInvariant())),
+  );
+}
+
+export default configureStore;
