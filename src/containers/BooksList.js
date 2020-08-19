5ee44f5667e0c709f270f8f87003801a91dd5deb
firmo holanda <firmo@viize.com>
2020-08-19 12:59:27 -0300

    refactor from milestone 2

---
+++ src/containers/BooksList.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import PropTypes from 'prop-types';
+
+import Book from '../components/Book';
+
+const BooksList = ({ books }) => (
+  <table className="table table-striped table-hover">
+    <thead>
+      <tr>
+        <th scope="col">id</th>
+        <th scope="col">title</th>
+        <th scope="col">category</th>
+      </tr>
+    </thead>
+    <tbody>
+      {books.map(book => (
+        <Book book={book} key={book.id} />
+      ))}
+    </tbody>
+  </table>
+);
+
+const mapStateToProps = (state) => ({ books: state.books, filter: state.filter });
+
+BooksList.defaultProps = {
+  books: [],
+};
+
+BooksList.propTypes = {
+  books: PropTypes.arrayOf(PropTypes.object),
+};
+
+export default connect(mapStateToProps)(BooksList);
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+import App from './components/App';
+import configureStore from './configureStore';
+import initialState from './rawData';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/styles/index.css';
+
+const store = configureStore(initialState);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
