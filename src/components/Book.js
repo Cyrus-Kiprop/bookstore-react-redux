b08142b211c5e20d2f41ca464e681ee96013d915
kip <cyruskiprop254@gmail.com>
2020-08-19 20:24:20 +0300

    :sparkles: Delete a Book
    add delete book functionality

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Book = ({ book }) => (
+const Book = ({ book, handleRemoveBook }) => (
   <tr>
     <td>{book.id.slice(0, 5)}</td>
     <td>{book.title}</td>
     <td>{book.category}</td>
     <td>
       {' '}
-      <button className="btn btn-danger btn-sm" type="button">remove book</button>
+      <button onClick={() => handleRemoveBook(book)} className="btn btn-danger btn-sm" type="button">remove book</button>
       {' '}
     </td>
   </tr>
@@ -20,6 +20,7 @@ Book.propTypes = {
     title: PropTypes.string.isRequired,
     category: PropTypes.string.isRequired,
   }).isRequired,
+  handleRemoveBook: PropTypes.func.isRequired,
 };
 
 export default Book;
